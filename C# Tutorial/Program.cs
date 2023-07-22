a909db6b46f77bd5c274702a488e06a708e48c89
Jasleen8801 <jkaur1_be21@thapar.edu>
2023-07-22 15:30:57 +0530

    c# done'

--- C# Tutorial/Program.cs
+++ C# Tutorial/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 namespace HelloWorld
 {
-    class Program {
-        public static void _HelloWorld() {
-          Console.WriteLine("Hello world!!");
-        }
-        
-        public static void Main(string[] args) {
-          _HelloWorld();
-        }
+  class Person {
+    public string name;
+    public int age;
+
+    public Person() {
+      name = "Hello World";
+      age = 18;
     }
+
+    public Person(string _name, int _age) {
+      name = _name;
+      age = _age;
+    }
+
+    public void GetDetails() {
+      Console.WriteLine("Name: " + name);
+      Console.WriteLine("Age: " + age);
+    }
+  }
+
+  class Program {        
+     public static void Main(string[] args) {
+       Person P = new Person("Jake", 24);
+       P.GetDetails();
+     }
+   }
 }
 
 // to run: dotnet build && dotnet run
